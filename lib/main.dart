b5dad161a6881b22db0b30dc02ef6f8401d8c95c
Waleed-Sheikh210 <94347260+Waleed-Sheikh210@users.noreply.github.com>
2021-11-30 11:07:39 +0500

    QR scan

---
+++ lib/main.dart
@@ -0,0 +1,188 @@
+
+import 'dart:developer';
+import 'dart:io';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:qr_code_scanner/qr_code_scanner.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+      
+        primarySwatch: Colors.blue,
+      ),
+      home: Go()
+      
+    );
+  }
+}
+
+class Go extends StatefulWidget {
+  const Go({ Key? key }) : super(key: key);
+
+  @override
+  _GoState createState() => _GoState();
+}
+
+class _GoState extends State<Go> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+     body: Center(
+        child: ElevatedButton(onPressed: (){Navigator.of(context).push(MaterialPageRoute(builder: (context)=>QRViewExample()));}, child: Text("QR View")),
+    ));
+  }
+}
+
+class QRViewExample extends StatefulWidget {
+  const QRViewExample({ Key? key }) : super(key: key);
+
+  @override
+  _QRViewExampleState createState() => _QRViewExampleState();
+}
+
+class _QRViewExampleState extends State<QRViewExample> {
+
+Barcode? result;
+QRViewController? controller;
+final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
+
+@override
+  void reassemble() {
+    super.reassemble();
+    if (Platform.isAndroid) {
+      controller!.pauseCamera();
+    }
+    controller!.resumeCamera();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+    body: Column(children: [
+      Expanded(
+        child: _buildQrView(context),
+        flex: 4,),
+        Expanded(
+          flex: 1,
+          child: FittedBox(
+            fit: BoxFit.contain,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  if (result != null) 
+                    Text('Barcode Type: ${describeEnum(result!.format)} Data: ${result!.code}')
+                  else 
+                  const Text("Scan a code"),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Container(
+                        margin: const EdgeInsets.all(8),
+                        child: ElevatedButton(onPressed: ()async{
+                          await controller?.toggleFlash();
+                          setState(() {});
+                        }, child: FutureBuilder(
+                          future: controller?.getFlashStatus(),
+                          builder: (context, AsyncSnapshot snapshot){
+                            return Text('Flash: ${snapshot.data}');
+                          }
+                          )),
+                      ),
+                      Container(
+                        margin: EdgeInsets.all(8),
+                        child: ElevatedButton(onPressed: ()async{
+                          await controller?.flipCamera();
+                          setState(() {});
+                        }, child: FutureBuilder(builder: (context, AsyncSnapshot snapshot){
+                          if (snapshot.data != null) {
+                            return Text('Camera Facing ${describeEnum(snapshot.data)}');
+                          }else{
+                            return CircularProgressIndicator();
+                          }
+                        })),
+                      )
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Container(
+                        margin: EdgeInsets.all(8),
+                        child: ElevatedButton(onPressed: ()async{
+                          await controller?.pauseCamera();
+                        }, child: const Text("pause",style: TextStyle(fontSize: 20),)),
+                      ),
+                       Container(
+                        margin: const EdgeInsets.all(8),
+                        child: ElevatedButton(
+                          onPressed: () async {
+                            await controller?.resumeCamera();
+                          },
+                          child: const Text('resume',
+                              style: TextStyle(fontSize: 20)),
+                        ),
+                       )],
+                  )
+              ],
+              ),
+          )),
+
+    ],),
+    );
+  }
+  
+Widget _buildQrView(BuildContext context){
+ var scanArea = (MediaQuery.of(context).size.width < 400 ||
+            MediaQuery.of(context).size.height < 400)
+        ? 150.0
+        : 300.0;
+        
+        return QRView(
+          key: qrKey,
+          onQRViewCreated: _onQRViewCreated,
+          overlay:  QrScannerOverlayShape(
+            borderColor: Colors.red,
+            borderRadius: 10,
+            borderLength: 30,
+            borderWidth: 10,
+            cutOutSize: scanArea
+          ),
+          onPermissionSet: (ctlr, p) => _onPermissionSet(context, ctlr, p),
+          );
+        }
+        void _onQRViewCreated (QRViewController controller){
+          setState(() {
+            this.controller = controller;
+          });
+          controller.scannedDataStream.listen((scanData) {
+            setState(() {
+              result = scanData;
+            });
+          });
+        }
+        void _onPermissionSet(BuildContext context, QRViewController ctrl , bool p){
+          log('${DateTime.now().toIso8601String()}_onPermissionSet $p');
+          if (!p) {
+            ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('no Permission')));
+          }
+        }
+
+        void dispose(){
+          controller?.dispose();
+          super.dispose();
+        }
+}
+
